4220fa11a5ac9386a3c0ddd3008be1560fc06b6a
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-01-14 19:28:54 -0800

    More attemps to fix load order

--- lib/active_record/write.rb
+++ lib/active_record/write.rb
@@ -1,4 +1,12 @@
 module ActiveRecord
+  module ActiveRecord
+    class Base
+      def self.write(columns:, query: self, target: self.table_name, size: Write::DEFAULT_SIZE, serializer: Write::DEFAULT_SERIALIZER, &iteration)
+        ActiveRecord::Write.new(columns: columns, query: query, target: target, size: size, serializer: serializer, &iteration).pool
+      end
+    end
+  end
+
   class Write
     require_relative "write/version"
 
