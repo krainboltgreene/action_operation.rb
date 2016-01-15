596efe4437b05471d13c8c441a8cede7031a06c6
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-01-14 19:29:16 -0800

    Fixing scope

--- lib/active_record/write.rb
+++ lib/active_record/write.rb
@@ -1,9 +1,7 @@
 module ActiveRecord
-  module ActiveRecord
-    class Base
-      def self.write(columns:, query: self, target: self.table_name, size: Write::DEFAULT_SIZE, serializer: Write::DEFAULT_SERIALIZER, &iteration)
-        ActiveRecord::Write.new(columns: columns, query: query, target: target, size: size, serializer: serializer, &iteration).pool
-      end
+  class Base
+    def self.write(columns:, query: self, target: self.table_name, size: Write::DEFAULT_SIZE, serializer: Write::DEFAULT_SERIALIZER, &iteration)
+      ActiveRecord::Write.new(columns: columns, query: query, target: target, size: size, serializer: serializer, &iteration).pool
     end
   end
 
